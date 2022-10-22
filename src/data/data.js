286ee1fcc641990065a531eebf8519757be9ae1f
Taulant Sulko <taulant.sulko@gmail.com>
2022-10-22 17:27:36 -0400

    Adding mock data.

---
+++ src/data/data.js
@@ -0,0 +1,156 @@
+const sellable = {
+  travel: {
+    company: "Greyhound",
+    vehicle: {
+      type: "Bus",
+      amenities: ["Wifi", "Bathroom", "ETicket"],
+    },
+    passengerCount: 1,
+    from: {
+      time: "8:00 am",
+      location: "Port Authority",
+      city: "New York City",
+      map: "#",
+    },
+    to: {
+      time: "12:00 pm",
+      location: "Union Station",
+      city: "Washington",
+      map: "#",
+    },
+    duration: {
+      time: "4h 30m",
+      note: null,
+    },
+    itinerary: false,
+  },
+
+  tickets: [
+    {
+      type: "Regular",
+      amenities: [],
+      price: "$45",
+    },
+  ],
+};
+
+const summary = {
+  travel: {
+    company: "Greyhound",
+    vehicle: {
+      type: "Bus",
+      amenities: ["Wifi", "Bathroom", "ETicket"],
+    },
+    passengerCount: 2,
+    from: {
+      time: "8:00 am",
+      location: "Port Authority",
+      city: "New York City",
+      map: "#",
+    },
+    to: {
+      time: "12:00 pm",
+      location: "Union Station",
+      city: "Washington",
+      map: "#",
+    },
+    duration: {
+      time: "4h 30m",
+      note: null,
+    },
+    itinerary: true,
+  },
+  tickets: [
+    {
+      type: "Regular",
+      amenities: [],
+      price: "$45",
+    },
+  ],
+};
+
+const fastest = {
+  travel: {
+    company: "Greyhound",
+    vehicle: {
+      type: "Bus",
+      amenities: ["Wifi", "Bathroom", "ETicket"],
+    },
+    passengerCount: 1,
+    from: {
+      time: "8:00 am",
+      location: "Port Authority",
+      city: "New York City",
+      map: "#",
+    },
+    to: {
+      time: "12:00 pm",
+      location: "Union Station",
+      city: "Washington",
+      map: "#",
+    },
+    duration: {
+      time: "4h 30m",
+      note: "Fastest",
+    },
+    itinerary: false,
+  },
+
+  tickets: [
+    {
+      type: "Regular",
+      amenities: [],
+      price: "$45",
+    },
+  ],
+};
+
+const moreOptions = {
+  travel: {
+    company: "Greyhound",
+    vehicle: {
+      type: "Bus",
+      amenities: ["Wifi", "Bathroom", "ETicket"],
+    },
+    passengerCount: 1,
+    from: {
+      time: "8:00 am",
+      location: "Port Authority",
+      city: "New York City",
+      map: "#",
+    },
+    to: {
+      time: "12:00 pm",
+      location: "Union Station",
+      city: "Washington",
+      map: "#",
+    },
+    duration: {
+      time: "4h 30m",
+      note: null,
+    },
+    itinerary: false,
+  },
+
+  tickets: [
+    {
+      type: "Regular",
+      amenities: [],
+      price: "$45",
+    },
+    {
+      type: "First-class",
+      amenities: ["Refund", "SeatXL", "Utensils"],
+      price: "$95",
+    },
+  ],
+};
+
+const data = {
+  sellable,
+  summary,
+  fastest,
+  moreOptions,
+};
+
+export default data;
